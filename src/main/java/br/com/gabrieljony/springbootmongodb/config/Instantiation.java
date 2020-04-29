3ab23122954783319815bc170e6c3bb445637c00
Gabriel Jony <gabjony@gmail.com>
2020-04-29 13:41:35 -0300

    9 Criando entity Post com User aninhado

--- src/main/java/br/com/gabrieljony/springbootmongodb/config/Instantiation.java
+++ src/main/java/br/com/gabrieljony/springbootmongodb/config/Instantiation.java
@@ -1,6 +1,8 @@
 package br.com.gabrieljony.springbootmongodb.config;
 
+import br.com.gabrieljony.springbootmongodb.domain.Post;
 import br.com.gabrieljony.springbootmongodb.domain.User;
+import br.com.gabrieljony.springbootmongodb.repository.PostRepository;
 import br.com.gabrieljony.springbootmongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,15 +16,25 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post = new Post(null, System.currentTimeMillis(), "Partiu Viagem", "Descrição do post de partiu viagem.", maria);
+        Post post2 = new Post(null, System.currentTimeMillis(), "Partiu Viagem", "Descrição do post de partiu viagem.", alex);
+        Post post3 = new Post(null, System.currentTimeMillis(), "Partiu Viagem", "Descrição do post de partiu viagem.", bob);
+        Post post4 = new Post(null, System.currentTimeMillis(), "Partiu Viagem", "Descrição do post de partiu viagem.", maria);
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post, post2, post3, post4));
     }
 }
