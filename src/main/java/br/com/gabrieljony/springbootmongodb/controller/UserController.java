410a4dd6312625ce7f83e46d991daad7f12f3b07
Gabriel Jony <gabjony@gmail.com>
2020-04-29 12:16:54 -0300

    5 Obtendo um usuário por id e realizando o tratamento de exceção caso tenha um id errado. Error 404.

--- src/main/java/br/com/gabrieljony/springbootmongodb/controller/UserController.java
+++ src/main/java/br/com/gabrieljony/springbootmongodb/controller/UserController.java
@@ -5,6 +5,7 @@ import br.com.gabrieljony.springbootmongodb.dto.UserDTO;
 import br.com.gabrieljony.springbootmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,12 @@ public class UserController {
         List<User> list = userService.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 
 }
