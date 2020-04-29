4e4af1b77818e78a605c71e6d95b368577790716
Gabriel Jony <gabjony@gmail.com>
2020-04-29 18:06:05 -0300

    15 Consulta simples com query methods Referências: https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/ https://docs.spring.io/spring-data/data-document/docs/current/reference/html/ Em PostRepository, criar o método de busca Em PostService, criar o método de busca No subpacote resources.util, criar classe utilitária URL com um método para decodificar parâmetro de URL Em PostResource, implementar o endpoint

--- src/main/java/br/com/gabrieljony/springbootmongodb/controller/PostController.java
+++ src/main/java/br/com/gabrieljony/springbootmongodb/controller/PostController.java
@@ -1,10 +1,12 @@
 package br.com.gabrieljony.springbootmongodb.controller;
 
+import br.com.gabrieljony.springbootmongodb.controller.util.URL;
 import br.com.gabrieljony.springbootmongodb.domain.Post;
 import br.com.gabrieljony.springbootmongodb.domain.User;
 import br.com.gabrieljony.springbootmongodb.dto.UserDTO;
 import br.com.gabrieljony.springbootmongodb.services.PostService;
 import br.com.gabrieljony.springbootmongodb.services.UserService;
+import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -31,5 +33,12 @@ public class PostController {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = postService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/br/com/gabrieljony/springbootmongodb/repository/PostRepository.java
+++ src/main/java/br/com/gabrieljony/springbootmongodb/repository/PostRepository.java
@@ -4,6 +4,11 @@ import br.com.gabrieljony.springbootmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+//    https://docs.spring.io/spring-data/data-document/docs/current/reference/html/#repositories.query-methods
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
